7a790c5ea79f88518dbadb12133d1cf538566548
Norman G <normang@1qwe.de>
2024-03-10 13:55:15 +0100

    add UtkBase utility functions for generic things like padding, alignment and others

--- UtkBase/utility.py
+++ UtkBase/utility.py
@@ -36,3 +36,55 @@ def diffBinary(binA: bytes, binB: bytes) -> bool:
         offset += 256
 
     return equals
+
+
+def fillBinaryTill(binary: bytes, targetSize: int, filler: bytes = b'\xFF') -> bytes:
+    """
+    Fills the given binary with a filler-value until it is as large as the target-size.
+
+    Useful for creating paddings between things.
+    The Padding or Filler value defaults to 0xFF but can be optionally set to what ever is desired.
+    0xFF and 0x00 make the most sense as the filler-value.
+
+    :param binary: Binary to add filler / padding to
+    :param targetSize: Size of the Binary to fill it up to
+    :param filler: A single byte to be repeatedly added to the given binary. 0xFF and 0x00 make the most sense. By default, b'\xFF'.
+    :return: Binary of the target-size
+    """
+    amountOfFiller = targetSize - len(binary)
+    assert amountOfFiller >= 0, "Binary larger then requested Size of {}, actual size {}\n Can't shrink the Binary here".format(hex(targetSize), hex(len(binary)))
+    if 0 == amountOfFiller:
+        return binary
+    fillerBin = amountOfFiller * filler
+    binary += fillerBin
+    return binary
+
+
+def alignOffset(offset: int, alignment: int) -> int:
+    """
+    Align the offset with the given alignment
+
+    Use for Offset and padding calculations when there are specific alignment requirements like block-sizes.
+
+    :param offset: offset to be aligned
+    :param alignment: Alignment-Number to align the offset with
+    :return: the new offset after alignment
+    """
+    alignedOffset = offset + ((alignment - (offset & (alignment - 1))) & (alignment - 1))
+    return alignedOffset
+
+
+def alignOffsetDown(offset: int, alignment: int) -> int:
+    """
+    Align the offset 'down' to a smaller number with the given alignment
+
+    Use this for example when building binaries from the back to the front.
+
+    :param offset: offset to be aligned
+    :param alignment: Alignment-Number to align the offset with
+    :return: the new offset after alignment
+    """
+    mask = alignment - 1
+    invertedMask = mask.__invert__()
+    newAddress = offset & invertedMask
+    return newAddress
