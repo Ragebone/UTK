f5981a686d5ad85d5945907419f7388c8dc053e1
Norman G <normang@1qwe.de>
2024-03-18 00:28:49 +0100

    Improve Uefi Volume - Header implementation
    With comments
    A validate function to calculate the Checksum
    General code cleanup

--- UtkBase/images/volumes/headers/volumeHeader.py
+++ UtkBase/images/volumes/headers/volumeHeader.py
@@ -1,14 +1,40 @@
-import logging
 import struct
+import textwrap
 
-from UtkBase.images.volumes.headers.blockMap import Block
 from UtkBase.uefiGuid import UefiGuid
+from UtkBase.utility import calculateChecksum16
 
 
 class VolumeHeader:
+    """
+    Represents the header at the start of a UEFI Volume.
+    The header consists of a structure and the so called Block-Map.
+    A checksum only verifies this header.
+
+    It can be easily identified in binary by the 16 leading 0es (Zero Vector) and
+    a *Magic* value of '_FVH' following a bit later.
+    """
 
     @classmethod
     def _struct(cls) -> struct:
+        """
+        https://github.com/LongSoft/UEFITool/blob/c5508535c135612dc921ae0d38eb0cfaae2d33d4/common/ffs.h#L111
+        // Volume header
+        typedef struct EFI_FIRMWARE_VOLUME_HEADER_ {
+            UINT8    ZeroVector[16];
+            EFI_GUID FileSystemGuid;
+            UINT64   FvLength;
+            UINT32   Signature;
+            UINT32   Attributes;
+            UINT16   HeaderLength;
+            UINT16   Checksum;
+            UINT16   ExtHeaderOffset;  //Reserved in Revision 1
+            UINT8    Reserved;
+            UINT8    Revision;
+            //EFI_FV_BLOCK_MAP_ENTRY FvBlockMap[2];
+        } EFI_FIRMWARE_VOLUME_HEADER;
+        """
+        # TODO handle version 1 as is stated above
         return struct.Struct('<16s 16s q 4s I H H H B B')
 
     @classmethod
@@ -27,8 +53,11 @@ class VolumeHeader:
             revision
         ) = cls._struct().unpack(STRUCT_BINARY)
 
-        assert ZERO_VECTOR == b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00', "Reserved must be 16 Bytes of 0s, got {}".format(ZERO_VECTOR.hex().upper())
-        assert magic == b'_FVH', "Must have _FVH as magic, got {}".format(magic)
+        assert ZERO_VECTOR == b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00', f"""
+        Reserved must be 16 Bytes of 0s, got {ZERO_VECTOR.hex().upper()}
+        """
+
+        assert magic == b'_FVH', "Volume header must have '_FVH' as magic, got {}".format(magic)
 
         # limit header binary to its structure and blockMap
         HEADER_BINARY = binary[:headerSize]
@@ -37,8 +66,18 @@ class VolumeHeader:
 
         BLOCK_MAP_BINARY = HEADER_BINARY[cls._struct().size:]
 
-        header = cls(guid, volumeSize, attributeMask, headerSize, checksum, externalHeaderOffset,
-                     reserved, revision, BLOCK_MAP_BINARY)
+        header = cls(
+            guid,
+            volumeSize,
+            attributeMask,
+            headerSize,
+            checksum,
+            externalHeaderOffset,
+            reserved,
+            revision,
+            BLOCK_MAP_BINARY
+        )
+
         return header
 
     def __init__(self, guid: UefiGuid, volumeSize, attributeMask, headerSize, checksum, externalHeaderOffset, reserved, revision, blockMapBinary):
@@ -46,23 +85,50 @@ class VolumeHeader:
         self._volumeSize = volumeSize
         self._attributeMask = attributeMask
         self._headerSize = headerSize                                   # Includes the BlockMap
-        self._checksum = checksum
+        self._checksum = 0                                              # validates only this header, not the volume. Includes blockMap and zero vector
         self._externalHeaderOffset = externalHeaderOffset
         self._reserved = reserved
-        self._revision = revision
+        self._revision = revision                                       # TODO check for the Revision, maybe even in the factory
 
+        # TODO add blockMap parsing
         self._blockMapBinary = blockMapBinary
 
+        # validate correctness
+        self._checksum = self.validate(checksum)
+
+    def validate(self, CHECKSUM: int = None, updateCheckSum: bool = False) -> bool:
+        """
+        # TODO really start using this and stuff, this might suck as it is right now
+        Validate the header by recalculating and comparing checksums
+
+        :param: CHECKSUM the parsed checksum to validate and reference, None if ignored
+        :return: the calculated checksum
+        """
+        self._checksum = 0
+        CURRENT_CHECKSUM = calculateChecksum16(self.serialize())
+
+        if CHECKSUM is not None:
+            assert CHECKSUM == CURRENT_CHECKSUM, "Checksums do not match, current {}, expected {]".format(CURRENT_CHECKSUM, CHECKSUM)
+
+        if updateCheckSum:
+            self._checksum = CURRENT_CHECKSUM
+
+        return CURRENT_CHECKSUM
+
     def getGuid(self) -> UefiGuid:
+        """Get the UEFI GUID object of the header"""
         return self._guid
 
     def getSize(self) -> int:
+        """Size of this header including blockMap and zero-vector"""
         return self._headerSize
 
     def getVolumeSize(self) -> int:
+        """Size of the UEFI volume, header included"""
         return self._volumeSize
 
     def getExternalHeaderOffset(self) -> int:
+        """Offset the External Header Structure is located at"""
         return self._externalHeaderOffset
 
     def toString(self) -> str:
