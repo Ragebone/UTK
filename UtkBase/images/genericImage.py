4f361f036f9163ed64d8bea756a02e17bff632e7
Norman G <normang@1qwe.de>
2024-03-18 00:33:38 +0100

    add commentary and docstrings to the Generic UEFI image implementation

--- UtkBase/images/genericImage.py
+++ UtkBase/images/genericImage.py
@@ -5,6 +5,17 @@ from UtkBase.images.image import Image
 
 
 class GenericImage(Image):
+    """
+    A generic UEFI image implementation assuming that an image is just UEFI Volumes and possibly paddings inbetween.
+    Hence, this features just a List of 'ImageElement's those being UEFI Volumes and Padding objects.
+    This does not feature any understanding or capability for any OEM.
+
+    Useful as a reference and fallback in case something goes wrong with an OEM image.
+    Or for general testing and getting something up and running quickly, like this project here.
+
+    AMD based images are a bit more complex with the PSP firmware structures.
+    Intel based images similarly so.
+    """
 
     @classmethod
     def fromImageElements(cls, contents: List[ImageElement]) -> Image:
@@ -16,6 +27,7 @@ class GenericImage(Image):
         return cls(contents)
 
     def __init__(self, contents=None):
+        # TODO consider making this a dictionary with the key being the offset inside the image
         self._contents: List[ImageElement] = [] if contents is None else contents
 
     def getSize(self):
@@ -44,6 +56,7 @@ class GenericImage(Image):
         return outString
 
     def serialize(self) -> bytes:
+        """ Serializable """
         imageBinary = bytes()
         for imageElement in self._contents:
             BINARY = imageElement.serialize()
