4377f6d228a233db36ea1023e78dd6c139b1c213
Norman G <normang@1qwe.de>
2024-04-27 18:27:32 +0200

    improve AMD EFS interface with comments

--- UtkAmd/uefi/images/psp/efs/efs.py
+++ UtkAmd/uefi/images/psp/efs/efs.py
@@ -2,23 +2,29 @@ import abc
 from typing import List
 
 from UtkAmd.utkAmdInterfaces import AMD
-# TODO this dependency is fine but should be pulled out somewhere else maybe?
 from UtkBase.images.imageElement import ImageElement
 
 
-class EmbeddedFirmwareStructure(ImageElement, AMD, abc.ABC):
+class EmbeddedFirmwareStructure(ImageElement, AMD):
     """
-    ABC / Interface for all EFSs
+    Interface all EFSs have to implement
     """
 
     @abc.abstractmethod
     def getOffset(self) -> int:
+        """Get the offset / location of the EFS"""
         pass
 
     @abc.abstractmethod
     def getSize(self) -> int:
+        """Get the size of the EFS in number of bytes"""
         pass
 
     @abc.abstractmethod
     def getDirectoryPointers(self) -> List[int]:
+        """
+        Get a list of all the offsets pointed at by the EFS that directories could be located at.
+
+        :return: List of int offsets relative to the start of the AmdImage
+        """
         pass
