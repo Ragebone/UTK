636becb9c0a584f96a7518afbefd081ab101623f
Norman G <normang@1qwe.de>
2024-04-28 14:08:57 +0200

    refactor UtkAmdInterface to now be UtkAMD

--- UtkAmd/utkAmdInterfaces.py
+++ UtkAmd/utkAmdInterfaces.py
@@ -1,5 +1,10 @@
-import abc
 
 
-class AMD:
+class UtkAMD:
+    """
+    Interface / class to bundle all AMD specific implementations under.
+
+    Use this for example to only print those things you are interested in.
+    See tools/amdInfo
+    """
     pass
