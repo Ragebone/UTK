4c1cbc89489210c5910332cc74a32a41efb121d7
Norman G <normang@1qwe.de>
2024-04-28 20:45:52 +0200

    cleanup and add to tools/common/conversions

--- tools/common/conversions.py
+++ tools/common/conversions.py
@@ -1,11 +1,43 @@
 import enum
-from typing import Any, List
 
 from utkInterfaces import Serializable
 
 
-def convertItem(item: Any, depth: int = 0):
-    if isinstance(item, Serializable):
+def recursiveToDict(dictionary: dict[str, any]) -> dict:
+    """
+
+    :param dictionary:
+    :return:
+    """
+    newDict = {}
+
+    for key, item in dictionary.items():
+
+        if isinstance(item, Serializable):
+            newDict[key] = recursiveToDict(item.toDict())
+            continue
+
+        if isinstance(item, int):
+            newDict[key] = hex(item)
+            continue
+
+        if isinstance(item, bytes):
+            binary: bytes = item
+            newDict[key] = binary.hex().upper()
+            continue
+
+        if isinstance(item, enum.Enum):
+            item: enum.Enum = item
+            newDict[key] = {
+                "name": item.name,
+                "value": item.value
+            }
+
+    return newDict
+
+
+def convertItem(item: any, depth: int = 0):
+    if isinstance(item, (Serializable)):
         if depth <= 0:
             return {
                 "class": item.__class__.__name__,
@@ -18,8 +50,8 @@ def convertItem(item: Any, depth: int = 0):
     if isinstance(item, dict):
         return convertDict(item, depth - 1)
 
-    if isinstance(item, List):
-        valueList: List = item
+    if isinstance(item, list):
+        valueList: list = item
         newList = []
         for listItem in valueList:
             newItem = convertItem(listItem, depth - 1)
@@ -39,7 +71,7 @@ def convertItem(item: Any, depth: int = 0):
     return item
 
 
-def convertDict(dictionary: dict[str, any], depth: int = 0) -> dict[str, Any]:
+def convertDict(dictionary: dict[str, any], depth: int = 0) -> dict[str, any]:
     outputDict = {}
     for key, value in dictionary.items():
         outputDict[key] = convertItem(value, depth - 1)
