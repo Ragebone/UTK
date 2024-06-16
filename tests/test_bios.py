1edf658c794661979bd32d015a45a647bf5a8003
Norman G <normang@1qwe.de>
2024-06-16 20:25:10 +0200

    improve the current test_bios.py to look at the images specific content.

--- tests/test_bios.py
+++ tests/test_bios.py
@@ -2,14 +2,15 @@ import pytest
 import glob
 
 from UtkBase.biosFile import BiosFile
+
 from UtkBase.utility import diffBinary
 
 
-TEST_BIOS_FILE_PATHS = glob.glob("./images/*")
+TEST_BIOS_FILE_PATHS = glob.glob("images/*")
 
 
 @pytest.mark.parametrize('filename', TEST_BIOS_FILE_PATHS)
-def test_bios(filename):
+def test_biosFile(filename):
     try:
         fileHandle = open(filename, 'rb')
     except Exception as ex:
@@ -21,8 +22,16 @@ def test_bios(filename):
 
     BiosFile.disableExceptionHandling()
     bios = BiosFile.fromBinary(BINARY)
-    serializedBios = bios.serialize()
 
-    assert len(BINARY) == len(serializedBios), "Length missmatch: {} where it should be {}".format(hex(len(BINARY)), hex(len(serializedBios)))
-    BINARIES_EQUAL = diffBinary(BINARY, serializedBios)
-    assert BINARIES_EQUAL, "Must equal"
+    images = bios.getImages()
+    for image in images:
+        imageOffset = image.getOffset()
+        imageContent = image.getContent()
+
+        for offset, imageElement in imageContent:
+            elementBinary = imageElement.serialize()
+            ABSOLUTE_ELEMENT_OFFSET = imageElement.getOffset() + imageOffset
+            actualBinary = BINARY[ABSOLUTE_ELEMENT_OFFSET:ABSOLUTE_ELEMENT_OFFSET + len(elementBinary)]
+            assert len(actualBinary) == len(elementBinary), "Length missmatch: {} where it should be {}".format(hex(len(elementBinary)), hex(len(actualBinary)))
+            assert diffBinary(actualBinary, elementBinary), "Must equal"
+
