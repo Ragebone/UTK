c00ed363bfddedaf1a7f5c7e8e4beae77734dc24
Norman G <norman@1qwe.de>
2026-08-18 15:24:38 +0000

    Move AMD PSP keyStore to psp/firmware

--- UtkAmd/psp/firmware/keyStore.py
+++
@@ -1,8 +0,0 @@
-from UtkAmd.psp.firmware.firmwareBlob import FirmwareBlob
-from UtkAmd.psp.firmwareTypes import FirmwareType
-from UtkCommon.interfaces.header import Header
-
-
-class KeyStore(FirmwareBlob):
-    def __init__(self, offset: int, binary: bytes, firmwareType: FirmwareType, header: Header = None):
-        super().__init__(offset, binary, firmwareType, header)
